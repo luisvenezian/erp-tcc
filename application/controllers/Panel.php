1872a6b9a1868434f6f0fcb5d15bd0abfb562f9a
venezianluis <felipelipe_11@hotmail.com>
2019-06-18 15:22:53 -0300

    Panel Model

--- application/controllers/Panel.php
+++ application/controllers/Panel.php
@@ -4,12 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Panel extends CI_Controller {
 
+	function __construct(){
+		parent::__construct();
+		$this->load->model('Panel_Model','bd');
+	}
+
 	public function index()
 	{
 		if (verify_logged() <> true){
 			redirect('login','refresh');
 		}
-		else
-		$this->load->view('panel');
+		else{
+			$user_session = getUserName();
+			$data = $this->bd->getApplicacoes($user_session);
+			$this->load->view('panel', $data);
+		}
 	}
 }
---
+++ application/models/Panel_Model.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Panel_Model extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function getApplicacoes($user)
+	{
+		$command_sql = "SELECT A.* 
+						FROM [users].[permission] AS P
+						JOIN [si].[application] AS A ON A.APP_ID = P.APP_ID
+						WHERE P.[user_name] = '".$user."'";
+		$query = $this->db->query($command_sql);
+
+		return $query->result();
+	}
+
+}
