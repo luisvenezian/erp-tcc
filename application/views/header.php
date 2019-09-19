e10ff30367fc038f9859ec73dd0927c279142099
venezianluis <felipelipe_11@hotmail.com>
2019-09-19 18:04:47 -0300

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -8,7 +8,7 @@
   <meta name="author" content="">
   <link rel="icon" href="application\img\icon-porco.png">
 
-  <title>ERP - ICORP</title>
+  <title>ERP - SUINOS</title>
 
   <!-- Principal CSS do Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
